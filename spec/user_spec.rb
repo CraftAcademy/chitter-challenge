3906adb0f1e1947e17b852921f7eebed7ba39e5b
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-09-24 22:09:36 +0200

    add user specs passing

---
+++ spec/user_spec.rb
@@ -0,0 +1,15 @@
+require 'user'
+
+describe User do
+
+  it { is_expected.to have_property :id   }
+  it { is_expected.to have_property :username   }
+  it { is_expected.to validate_uniqueness_of :username }
+  it { is_expected.to have_property :name   }
+  it { is_expected.to have_property :email   }
+  it { is_expected.to have_property :email   }
+  it { is_expected.to validate_uniqueness_of :email }
+  it { is_expected.to validate_presence_of :password }
+  it { is_expected.to validate_presence_of :password_confirmation }
+
+end
