14a397a502461e7e85f270d09031dec5b3ccd3ec
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-09-25 15:44:28 +0200

    try to fix heroku issue by adding dm auto migrate

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -15,8 +15,8 @@ class Chitter < Sinatra::Base
 
   DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{env}")
   DataMapper.finalize
-  DataMapper.auto_upgrade!
-  #DataMapper.auto_migrate!
+  #DataMapper.auto_upgrade!
+  DataMapper.auto_migrate!
   DataMapper::Model.raise_on_save_failure = true
 
   before do
