ae997bd70c8b3ae1fbd83a28096748fb27441009
David <dhesabian@gmail.com>
2015-10-13 11:59:58 +0200

    added body/max lenght to peep next: created by name

--- lib/peep.rb
+++ lib/peep.rb
@@ -5,8 +5,11 @@ class Peep
   include DataMapper::Resource
 
   property :id, Serial
-
+  property :body, Text
   property :created_at, DateTime
+
+  validates_presence_of :body
+  validates_length_of :body, :maximum => 200
 
 
 
