c3fcbaa4bb6e1c90be2cd07722f59bb961cb8c68
David <dhesabian@gmail.com>
2015-10-13 11:30:29 +0200

    learning from my mistakes, created a datetime for peep

--- lib/peep.rb
+++ lib/peep.rb
@@ -5,13 +5,9 @@ class Peep
   include DataMapper::Resource
 
   property :id, Serial
-  #property :content, String
-  #property :created_at, Datetime
-  #property :created_by_name. String
 
-  #belongs_to :user
+  property :created_at, DateTime
 
-  #validates_presence_of :peep_content
-  #validates_length_of :peep_content, :maximum => 200
+
 
 end
